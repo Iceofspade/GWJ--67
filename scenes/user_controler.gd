f496b43406ca8af0286010ed0714de4051f61f0f
HolyCatPL <erwindarul@gmail.com>
2024-03-16 23:44:17 +0100

    movement tweaks
    Ray moves even if unit didnt move, units cant collide in one cell

--- scenes/user_controler.gd
+++ scenes/user_controler.gd
@@ -1,12 +1,13 @@
 extends Node
 class_name User_Controles
 @export var unit:Unit
+var direction:Vector2i
 
 func _physics_process(_delta):
 	if unit.is_moving:
 		return
 	var tile_map = Globals.tilemap
-	var direction:Vector2i
+	var action_pressed = true
 	
 	if Input.is_action_pressed("move_R"):
 		direction = Vector2i.RIGHT
@@ -17,23 +18,24 @@ func _physics_process(_delta):
 	elif Input.is_action_pressed("move_UP"):
 		direction = Vector2i.UP
 	else :
-		direction = Vector2i.ZERO
+		action_pressed = false
 		
+	unit.ray_angle = ((direction as Vector2).angle())
 		
 	if unit.ray.get_collider():
-		if  unit.ray.get_collider().get_parent() is Unit:
+		if unit.ray.get_collider().get_parent() is Unit:
 			print(unit.ray.get_collider().get_parent())
-			unit.ray.rotation = ((direction as Vector2).angle()) 
 			unit.ray.get_collider().get_parent().emit_signal("gain_infection")
+			unit.ray.get_collider().get_parent().ray.rotation = unit.ray_angle
 			unit.emit_signal("lost_infection")
 			return
 					
-	if direction:
+	if action_pressed and !unit.infection_delay:
+		unit.ray.rotation = unit.ray_angle
 		if unit.can_move_there(direction + tile_map.local_to_map(unit.global_position)):
 			unit.target_pos = direction + tile_map.local_to_map(unit.global_position)
-			unit.ray_angle = ((direction as Vector2).angle()) 
 			# Auto step base infection
-
+			
 			unit.move_self()
 			# Uncomment for step based swap bodies 
 			Globals.emit_signal("step")
